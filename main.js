13d72a62bf48efe4f27bba2c66404bb4d8541ed8
eddanieles <ed.danieles@yahoo.com>
2016-07-26 16:15:31 -0400

    updated files

--- main.js
+++ main.js
@@ -3,14 +3,35 @@ var augend = document.querySelector("#number1");
 var addend = document.querySelector("#number2");
 
 
-function sum(augend, addend) {
+/* function sum(augend, addend) {
   var result = +augend + +addend;
   return(result);
-}
+} */
 
 var calculate = document.querySelector("button")
 
-calculate.addEventListener("clicked", sum());
 
+calculate.addEventListener("click", function(event){
+  var num1 = Number(augend.value);
+  var num2 = Number(addend.value);
+  var result = num1 + num2;
+  console.log(result);
+
+  var newItem = document.createElement("p");
+  newItem.innerText = result;
+  var answer = document.querySelector(".output");
+  answer.appendChild(newItem);
+});
+
+/*
+item.addEventListener("click", function(event){
+  var clickedItem = event.target;
+  var text = clickedItem.innerText;
+  var newItem = document.createElement("li");
+  newItem.innerText = text;
+  var list = document.querySelector("#list ul");
+  list.appendChild(newItem);
+});
+*/
 //var login = document.querySelector("#login");
 //console.log(login.value);
