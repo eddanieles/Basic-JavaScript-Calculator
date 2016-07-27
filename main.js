14ee0f5e96bf702188ad16df2ac4991287ad5a32
eddanieles <ed.danieles@yahoo.com>
2016-07-27 13:49:01 -0400

    updated files

--- main.js
+++ main.js
@@ -10,22 +10,24 @@ calculate.addEventListener("click", function(event){
 
   var num1 = Number(augend.value);
   var num2 = Number(addend.value);
-  var result = num1 + num2;
-  console.log(result);
   if (isNaN(num1) === true || isNaN(num1) === true) {
     alert("Hey, that is not a number ass hole!")
   };
-  var newItem = document.createElement("p");
-  newItem.innerText = result;
-  var answer = document.querySelector(".output");
-  answer.appendChild(newItem);
+  
+  var result = num1 + num2;
 
-  newItem.addEventListener('mousemove', function(event){
+//  var newItem = document.createElement("p");
+//  newItem.innerText = result;
+  var answer = document.querySelector(".output");
+  answer.innerText = result;
+  console.log(result);
+
+  answer.addEventListener('mousemove', function(event){
 
     var x = event.clientX;
     var y = event.clientY;
     //div.textContent = x + ', ' + y;
-    newItem.style.backgroundColor = 'rgb(' + x + ',' + y + ', 100)';
-    newItem.style.color = 'rgb(' + y + ',' + x + ', 100)'
+    answer.style.backgroundColor = 'rgb(' + x + ',' + y + ', 100)';
+    answer.style.color = 'rgb(' + y + ',' + x + ', 100)'
   });
 });
